cce35c670ec89ab27686a19ab3f6524bc00cd2c2
CLNorton <67613055+cingularities@users.noreply.github.com>
2023-02-16 13:40:04 -0700

    Delete SVM.R

--- SVM.R
+++
@@ -1,4 +0,0 @@
-svm <- svm(class~, data=NEON_indices, kernel ="linear")
-beginCluster(15)
-speciesspecific_svm_indices <- clusterR(NEON_indices, raster::predict, arcgs = (model = svm))
-endCluster()
